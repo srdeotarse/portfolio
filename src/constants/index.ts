e6de92b448542fd581a9ef9578ffb6a81336e69f
srdeotarse <srdeotarse@outlook.com>
2024-03-30 21:15:38 -0700

    Initial commit

---
+++ src/constants/index.ts
@@ -0,0 +1,253 @@
+import type {
+  TNavLink,
+  TService,
+  TTechnology,
+  TExperience,
+  TTestimonial,
+  TProject,
+} from "../types";
+
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  redhat,
+  gsoc,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks: TNavLink[] = [
+  {
+    id: "resume",
+    title: "Resume",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services: TService[] = [
+  {
+    title: "Software Developer",
+    icon: creator,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Open Source Developer",
+    icon: mobile,
+  },
+];
+
+const technologies: TTechnology[] = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences: TExperience[] = [
+  {
+    title: "Software Developer Intern",
+    companyName: "Red Hat",
+    icon: redhat,
+    iconBg: "#ffffff",
+    date: "August 2023 - December 2023",
+    points: [
+      "Enhanced Resource Optimization System (ROS), verified 4 important metrics of workload- CPU, memory, disk input/output-and compares these metrics to capacity allocation information.",
+      "Computed data from public cloud providers - AWS, Azure and Google Cloud to increase memory and CPU optimization by 15% under Red Hat Insights.",
+      "Managed overall inclusion of OpenAPI specification for ROS - OCP in another OpenAPI Specification file of Cost Management project resulting 20% growth in users.",
+      "Coded in programming languages - Python, Javascript, React, Go",
+    ],
+  },
+  {
+    title: "Open Source Contributor",
+    companyName: "Google Summer of Code @ Zulip",
+    icon: gsoc,
+    iconBg: "#ffffff",
+    date: "December 2021 - September 2022",
+    points: [
+      "Focused to Zulip Terminal, a TUI catering to users of Zulip through terminal. The project manifested Zulip Web App in terminal reducing loading time by 20% and improving performance by 15%.",
+      "Mentored fellow contributors in following Git commit practices and faster code review cycles.",
+      "Coded in programming languages- Python, pytest, JavaScript",
+    ],
+  },
+];
+
+const testimonials: TTestimonial[] = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects: TProject[] = [
+  {
+    name: "ChartWise",
+    description:
+      "Web-based platform for technical analysis of selected stock charts by displaying chart patterns - channel, triangle, head and shoulders and flag on candlestick stock chart.",
+    tags: [
+      {
+        name: "python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "flask",
+        color: "green-text-gradient",
+      },
+      {
+        name: "plotly",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    sourceCodeLink: "https://github.com/",
+  },
+  {
+    name: "Online Exam System",
+    description:
+      "Comprehensive system to tackle immediate problem of fair conduction of online exams during pandemic times using both Python and Java programming languages",
+    tags: [
+      {
+        name: "python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "flask",
+        color: "green-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    sourceCodeLink: "https://github.com/",
+  },
+  {
+    name: "Pshychologic Website",
+    description:
+      "A website showing a far-reaching approach towards present psychological needs to prevent fatal outcomes in life",
+    tags: [
+      {
+        name: "javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "html",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    sourceCodeLink: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
